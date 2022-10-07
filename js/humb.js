4960373a053f439d362ed63f8061561c95618765
NERON147 <faip98.07.5@gmail.com>
2022-10-07 21:09:06 +0400

    qq

---
+++ js/humb.js
@@ -0,0 +1,24 @@
+const popup = document.querySelector('#popup')
+const menuIcon = document.querySelector('.menu-icon')
+const menuWrapper = document.querySelector('.menu-icon-wrapper')
+const navi = document.querySelector('.pop__menu')
+
+
+
+menuWrapper.addEventListener("click", hambHndler);
+
+function hambHndler(e) {
+  e.preventDefault();
+  menuIcon.classList.toggle('menu-icon-active');
+  popup.classList.toggle('open');
+  document.body.classList.toggle('stop-scrolling');
+};
+
+navi.addEventListener('click', closeMenu)
+
+function closeMenu() {
+  popup.classList.remove('open');
+  document.body.classList.toggle('stop-scrolling');
+  menuIcon.classList.toggle('menu-icon-active');
+}
+
---
+++ js/main.js
@@ -0,0 +1,5 @@
+var cleave = new Cleave('.input-number', {
+    phone: true,
+    phoneRegionCode: 'RU',
+    delimiter: '-',
+  });
