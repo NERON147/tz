1e69f5492ecd5d6684e23bacf52168faef5a314c
NERON147 <faip98.07.5@gmail.com>
2022-10-07 21:31:22 +0400

    qq

--- js/main.js
+++ js/main.js
@@ -3,3 +3,7 @@ var cleave = new Cleave('.input-number', {
     phoneRegionCode: 'RU',
     delimiter: '-',
   });
+
+  ScrollReveal().reveal('.company-text', {  distance: '160px', origin: 'bottom', delay: 500, easing: 'ease-in',});
+  ScrollReveal().reveal('.work-text', {  distance: '160px', origin: 'bottom', delay: 1000, easing: 'ease-in', });
+  ScrollReveal().reveal('.help-text', {  distance: '160px', origin: 'bottom', delay: 1500, easing: 'ease-in', });
